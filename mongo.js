be56c67b49f40f40608dc2742ef885ec97dffd18
crholz <crholz@mtu.edu>
2020-10-21 11:47:05 -0400

    Added Database

--- mongo.js
+++ mongo.js
@@ -1,20 +1,10 @@
-const {MongoClient} = require("mongodb");
+const MongoClient = require("mongodb");
 
 const dburl = process.env.DB_URL;
 
 const dbClient =  new MongoClient(dburl);
 
-async function runDB() {
-    try {
-            await dbClient.connect();
-
-            const database = client.db('inviteToRole');
-    }
-
-    // DB = inviteRole
-    // coll = inviteTable
-
-    finally {
-        await clientInformation.close()
-    }
-}
+MongoClient.connect(dburl, function(err, db) {
+    if (err) throw err;
+    console.log("Connected to database.");
+});
